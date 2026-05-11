f736667a2732c3e6e0df643bfd8df2a95001b538
SkiBoxing <angrand.louis@gmail.com>
2026-05-11 12:15:29 +0200

    TaskResponse

--- src/main/java/org/example/dto/TaskResponse.java
+++ src/main/java/org/example/dto/TaskResponse.java
@@ -2,6 +2,8 @@ package org.example.dto;
 
 import org.example.entity.Task;
 
+import java.util.Date;
+
 /**
  * DTO de sortie.
  * Permet de controler ce que l'API renvoie.
@@ -9,26 +11,36 @@ import org.example.entity.Task;
 public class TaskResponse {
 
     private Long id;
-    private String name;
+    private String titre;
     private String description;
-    private boolean done;
+    private boolean priorite;
+    private String status;
+    private Date creationDate;
+    private Date deadline;
 
     public TaskResponse() {
     }
 
-    public TaskResponse(Long id, String name, String description, boolean done) {
+    public TaskResponse(Long id, String titre, String description, boolean priorite, String status, Date creationDate, Date deadline) {
         this.id = id;
-        this.name = name;
+        this.titre = titre;
         this.description = description;
-        this.done = done;
+        this.priorite = priorite;
+        this.status = status;
+        this.creationDate = creationDate;
+        this.deadline = deadline;
+
     }
 
     public static TaskResponse fromEntity(Task task) {
         return new TaskResponse(
-            task.getId(),
-            task.getName(),
-            task.getDescription(),
-            task.isDone()
+                task.getId(),
+                task.getTitre(),
+                task.getDescription(),
+                task.getPriorite(),
+                task.getStatus(),
+                task.getCreationDate(),
+                task.getDeadline()
         );
     }
 
@@ -36,15 +48,55 @@ public class TaskResponse {
         return id;
     }
 
-    public String getName() {
-        return name;
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public boolean isDone() {
-        return done;
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isPriorite() {
+        return priorite;
+    }
+
+    public void setPriorite(boolean priorite) {
+        this.priorite = priorite;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
     }
 }
