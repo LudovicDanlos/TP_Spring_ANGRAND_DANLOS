13e224271e0324888bd631789bccf80c1aecde81
LudovicD <danlos.ludo@gmail.com>
2026-05-11 12:16:47 +0200

    Adaptation du DTO d'entré TaskInput à l'entité Task.

--- src/main/java/org/example/dto/TaskInput.java
+++ src/main/java/org/example/dto/TaskInput.java
@@ -2,49 +2,70 @@ package org.example.dto;
 
 import org.example.entity.Task;
 
+import java.util.Date;
+
 /**
  * DTO de sortie.
  * Permet de controler ce que l'API renvoie.
  */
 public class TaskInput {
 
-    private Long id;
-    private String name;
+    private String titre;
     private String description;
-    private boolean done;
+
+    // Valeurs possibles LOW, MEDIUM, HIGH
+    private String priorite;
+
+    // Valeurs possibles TODO, IN_PROGRESS, DONE
+    private String status;
+
+    private Date creationDate;
+    private Date deadline;
 
     public TaskInput() {
     }
 
-    public TaskInput(Long id, String name, String description, boolean done) {
-        this.id = id;
-        this.name = name;
+    public TaskInput(String titre, String description, String priorite, String status, Date creationDate, Date deadline) {
+        this.titre = titre;
         this.description = description;
-        this.done = done;
+        this.priorite = priorite;
+        this.status = status;
+        this.creationDate = creationDate;
+        this.deadline = deadline;
     }
 
     public static TaskInput fromEntity(Task task) {
         return new TaskInput(
-            task.getId(),
-            task.getName(),
+            task.getTitre(),
             task.getDescription(),
-            task.isDone()
+            task.getPriorite(),
+            task.getStatus(),
+            task.getCreationDate(),
+            task.getDeadline()
         );
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
+    public String getTitre() {
+        return titre;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public boolean isDone() {
-        return done;
+    public String getPriorite() {
+        return priorite;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public Date getDeadline() {
+        return deadline;
     }
 }
